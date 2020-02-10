e949d6f18f16a1776230bb66ee1ccce147e935f0
Khanh Thuy Mai Nguyen <44103802+khanhthuymainguyen@users.noreply.github.com>
2020-02-11 01:27:18 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1FAQsgJGyJDyNUOh0XJ-oapv5F_2xyFRJ'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1IAMLTHKNfwkKatgt1umeSpYVfEEZul19'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['white-mushroom','brown-mushroom','poisonous-mushroom']
 path = Path(__file__).parent
 
 app = Starlette()
